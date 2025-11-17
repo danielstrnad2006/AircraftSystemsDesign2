ffafbb42654e47729934474b23e5efcc32f9d172
HapppyLance <happpy.lance@gmail.com>
2025-11-17 16:45:20 +0100

    new start

--- object.py
+++ object.py
@@ -1,14 +1,20 @@
 import math
+import scipy
 
-class span_pos(self):
-    def __init__(self, span, chord, Ai, Cl, lCd, Cm):
-        self.span = span # span-wise location
-        self.chord = chord
-        self.Ai = Ai # AOA, induced
-        self.Cl = Cl # Cl
-        self.lCd = lCd # induced cd
-        self.Cm = Cm # quarter chord moment
+from main import panel_size, _MISSING
 
-    def set_velocity(self, velocity):
-        self.velocity = velocity
-        self.f_l = 0.5 * chord
+class MissingDataError(Exception):
+    pass
+
+class span():
+    def __init__(self, y_pos, chord, Ai, Cl, lCd, Cm):
+        self.y_pos = scipy.interpolate(y_pos) # span-wise location
+        self.chord = scipy.interpolate(chord)
+        self.Ai = scipy.interpolate(Ai) # AOA, induced
+        self.Cl = scipy.interpolate(Cl) # Cl
+        self.lCd = scipy.interpolate(lCd) # induced cd
+        self.Cm = scipy.interpolate(Cm) # quarter chord moment
+
+    def cl(self):
+
+
