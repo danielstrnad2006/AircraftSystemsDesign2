aab145e0f7badafa624661baa09fc2b15844527e
moserjoki <moserjoki2003@gmail.com>
2025-11-19 17:32:34 +0100

    Spanwise

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ wing = WingSizing(S_w=149.9, b=32.1632, c_root=7.2855, c_tip=2.0372, taper_ratio
 
 db = 1
 b_cur = 0
-
+cross_sections = []
 while b_cur < wing.b/2:
 
     stiffeners = [
@@ -25,11 +25,12 @@ while b_cur < wing.b/2:
         (0.5, 'down', 400),
     ]
 
-    cross_section1 = CrossSection(xc_spar1=0.2, xc_spar2=0.6, chord=wing.chord(b_cur)*1000,
+    cs = CrossSection(xc_spar1=0.2, xc_spar2=0.6, chord=wing.chord(b_cur)*1000,
                                 t_spar1=50, t_spar2=50,
                                 t_skin_up=50, t_skin_down=50, stiffeners=stiffeners,
                                 filepath="airfoils/NASA SC(2)-0414.dat")
 
-    cross_section1.assembly_centroid_finder()
+    cs.assembly_centroid_finder()
+    cross_sections.append(cs)
 
     b_cur = b_cur + db
