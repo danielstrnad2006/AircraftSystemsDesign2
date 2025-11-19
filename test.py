d41d3217ced025fe14ce1a8dc81266919b133c2d
moserjoki <moserjoki2003@gmail.com>
2025-11-19 17:27:31 +0100

    Added change along span Co-authored-by: CanEzio <CanEzio@users.noreply.github.com> Co-authored-by: guodaost <guodaost@users.noreply.github.com>

--- test.py
+++ test.py
@@ -7,21 +7,29 @@ from cross_section import *
 from wing import *
 
 
+wing = WingSizing(S_w=149.9, b=32.1632, c_root=7.2855, c_tip=2.0372, taper_ratio=0.2796,
+                 leading_sweep=37.537, quart_sweep=34.4871, dihedral=5)
 
 
-stiffeners = [
-    (0.3, 'up', 400),
-    (0.3, 'down', 400),
-    (0.4, 'down', 400),
-    (0.5, 'down', 400),
-    (0.6, 'down', 400),
-]
+db = 1
+b_cur = 0
 
-wing = WingSizing
+while b_cur < wing.b/2:
 
-cross_section1 = CrossSection(xc_spar1=0.2, xc_spar2=0.6, chord=500,
-                              t_spar1=5, t_spar2=5,
-                              t_skin_up=5, t_skin_down=5, stiffeners=stiffeners,
-                              filepath="airfoils/NASA SC(2)-0414.dat")
+    stiffeners = [
+        (0.3, 'up', 400),
+        (0.4, 'up', 400),
+        (0.5, 'up', 400),
+        (0.3, 'down', 400),
+        (0.4, 'down', 400),
+        (0.5, 'down', 400),
+    ]
 
-cross_section1.assembly_centroid_finder()
+    cross_section1 = CrossSection(xc_spar1=0.2, xc_spar2=0.6, chord=wing.chord(b_cur)*1000,
+                                t_spar1=50, t_spar2=50,
+                                t_skin_up=50, t_skin_down=50, stiffeners=stiffeners,
+                                filepath="airfoils/NASA SC(2)-0414.dat")
+
+    cross_section1.assembly_centroid_finder()
+
+    b_cur = b_cur + db
