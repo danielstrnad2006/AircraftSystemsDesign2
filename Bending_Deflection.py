160253d5605742c2c094af30bb708d752a3e1384
guoda <guoda.ostrauskaite1@gmail.com>
2025-11-24 14:17:07 +0100

    update v-n code

---
+++ Bending_Deflection.py
@@ -0,0 +1,45 @@
+import numpy as np
+import scipy.integrate as sp
+import matplotlib.pyplot as plt
+
+#PARAMETERS/FUNCTION DEF
+def Mx(y):
+
+    return 10000 * (1 - y)   # Random
+
+def EIxx(y):
+
+    return 5e6 * (0.7 + 0.3*y)   # Random
+
+#FIRST INTEGRAND
+def dv_dy(y):
+    integrand = lambda s: Mx(s) / EIxx(s)
+    result, _ = sp.quad(integrand, 0, y)
+    return -result
+
+#SECOND INTEGRAND
+def v(y):
+    result, _ = sp.quad(dv_dy, 0, y)
+    return result
+
+# Span (normalised 0–1)
+y_span = np.linspace(0, 1, 200)
+
+# Compute slope and deflection
+dv_span = [dv_dy(y) for y in y_span]
+v_span = [v(y) for y in y_span]
+
+# Plot
+plt.figure()
+plt.plot(y_span, dv_span)
+plt.xlabel("y")
+plt.ylabel("dv/dy")
+plt.title("Slope distribution")
+
+plt.figure()
+plt.plot(y_span, v_span)
+plt.xlabel("y")
+plt.ylabel("v(y)")
+plt.title("Deflection distribution")
+
+plt.show()
