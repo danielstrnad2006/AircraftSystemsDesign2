99f7d4d79ef24d736164d3f9f04d4c4e8f6bd09f
moserjoki <moserjoki2003@gmail.com>
2025-11-26 17:05:30 +0100

    asdfasdf

--- internal_loads.py
+++ internal_loads.py
@@ -141,7 +141,7 @@ class HalfWing:
         self.torsion_distribtuion =  lambda y:  (self.x_centroid_distance(y)-self.x_cp_distance(y))*self.Lift(y)
         self.reaction_torsion = self.integrate_halfspan(self.internal_torsion)(self.b/2)
         self.internal_torsion = lambda y: self.integrate_halfspan(self.internal_torsion)(y) - self.reaction_torsion
-        self.internal_torsion = self.function_to_intrp1d(self.internal_torque)
+        self.internal_torsion = self.function_to_intrp1d(self.internal_torsion)
 
 
     def torque_per_span(self,y):
