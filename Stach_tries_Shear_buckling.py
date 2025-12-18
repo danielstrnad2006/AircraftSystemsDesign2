dc80df39005055a0dd59a511fa56b1fc1ac9f0ae
Stach <svleeshouwers23@gmail.com>
2025-12-18 12:32:28 +0100

    buckling graphs validation

--- Stach_tries_Shear_buckling.py
+++ Stach_tries_Shear_buckling.py
@@ -2,10 +2,8 @@ import math
 from planform import * 
 import scipy as sp
 
-
 wing = WingSizing(S_w=149.9, b=32.1632, c_root=7.2855, c_tip=2.0372, taper_ratio=0.2796,
                  leading_sweep=37.537, quart_sweep=34.4871, dihedral=5)
-
 
 k_s_values = [
         (1, 1.05, 15),
@@ -32,7 +30,6 @@ k_s_values = [
     ]
 
 def aspect_to_K(aspect_ratio):
-    print(aspect_ratio)
     for i in k_s_values:
         if i[0] <= aspect_ratio < i[1]:
             return i[2]
@@ -56,29 +53,44 @@ def shear_buckling_along(ribs, shear_stresses, thicknesses):
         if i!=0:
             b_spar =  0.14 * wing.chord(ribs[i-1])  #conservative assumption, takes the chord length at that location
         else: b_spar =0.14*wing.chord(0)
-        aspect_ratio = length/b_spar
+        aspect_ratio = (length/b_spar)
         K_value=aspect_to_K(aspect_ratio)
-        tau_crit = math.pi**2*K_value*E_mod/(12*(1-poisson**2))*(thicknesses[i]/b_spar)**2
-        output.append(tau_crit/shear_stresses[i])
+        tau_crit = (math.pi**2*K_value*E_mod/(12*(1-poisson**2))*(thicknesses[i]/b_spar)**2)*10**(-6)
+        safety=tau_crit/shear_stresses[i]
+        if safety>20:safety=20
+        output.append(safety)
     x_data.pop(-1)
+    
     graph = sp.interpolate.interp1d(x_data,output,kind="previous",bounds_error=False,fill_value=(output[0], output[-1]))
-    x_plot = np.linspace(0, wing.b/2, 300)
-    sf_plot = graph(x_plot)
-    plt.plot(x_plot, sf_plot)
-    plt.xlabel("Spanwise location y [m]")
-    plt.ylabel("Shear Buckling Margin Of Safety[-]")
-    plt.grid(True)
-    plt.show()
+    return output, graph
+
+##input all of this to find the graphs for safety buckling 
+ribs= [0, 2, 4, 6, 7.03893, 9, 12]  #rib locations along span of beam
+shear_stresses= [np.float64(1334.2858661244402), np.float64(1142.9765962870895), np.float64(949.8923116195851), np.float64(765.7974376040061), np.float64(675.4898938320113), np.float64(457.8899650787215), np.float64(177.3924126996869)]  #maximum shear stress along that section between the ribs MPA
+thicknesses=[0.05,0.05,0.05,0.03,0.03,0.02,0.02] #metres
 
 
-   ##input all of this to find the graphs for safety buckling 
-ribs= [0, 3, 6, 9, 12] #rib locations along span of beam
-shear_stresses=[10**9,2*10**9,3*10**9,4*10**9,5*10**9]  #maximum shear stress along that section between the ribs
-thicknesses=[0.001,0.001,0.001,0.001,0.001]
+safety_Factor=1.2
+count=50
+tolerance=0.05
+for _ in range(count):
+    values, useless = shear_buckling_along(ribs, shear_stresses, thicknesses)
 
-shear_buckling_along(ribs,shear_stresses, thicknesses)
+    for k, MS in enumerate(values):
 
+        if MS > safety_Factor + tolerance:
+            thicknesses[k] -= 0.001
+        elif MS < safety_Factor:
+            thicknesses[k] += 0.001
 
+values,graph= shear_buckling_along(ribs, shear_stresses, thicknesses)
+x_plot = np.linspace(0, wing.b/2, 300)
+sf_plot = graph(x_plot)
+plt.plot(x_plot, sf_plot)
+plt.xlabel("Spanwise location y [m]")
+plt.ylabel("Shear Buckling Margin Of Safety[-]")
+plt.grid(True)
+plt.show()
 
-
+print(thicknesses)
 
--- column_buckling.py
+++ column_buckling.py
@@ -19,31 +19,34 @@ def stringer_Ixx(area, ratio, thickness):
     I_xx=1/12*L*thickness**3+L*thickness*x_bar**2+1/12*thickness*B**3+thickness*B*(B-x_bar)**2
     return I_xx
 
-#Code to check safety factors across length of beam
-ribs= [0, 3, 6, 9, 12] #rib locations along span of beam
-normal_stresses=[5*10**9,10**9,2*10**9,4*10**9,5*10**9] #max normal stress for each segment
 
-#here you can change the parameters of the stringers along the section
+#input section
+ribs=  [0, 2, 4, 6, 7.03893, 9, 12] #rib locations along span of beam [m] # assuming the end of the wing has a rib
 
-stringer_area=[0.005,0.005,0.005,0.005,0.005] #area of each stringer in m^2
-stringer_ratios=[1.2,1.2,1.2,1.3,1.3]  #ratio is L/B where L is the stringer part that is parallel to the x-axis and B is the stringer length 
-stringer_thicknesses=[0.001,0.001,0.001,0.001,0.001] #m 
+normal_stresses=[np.float64(-221.55436933426083), np.float64(-184.66710646372886), np.float64(-149.41670185138847), np.float64(-132.10259507082256), np.float64(-90.00072765002402), np.float64(-33.40312927820247), np.float64(3.5356739692271065e-13)] #max normal stress for each segment
+#this is for each stringer, so if you want to change the stringer from 0 to 1st rib, change the first value 
+stringer_area=[0.000181,0.000181,0.000181,0.000181,0.000181,0.000181,0.000181,0.000181] #area of each stringer in m^2
+stringer_ratios=[1,1,1,1,1,1,1,1]  #ratio is L/B where L is the stringer part that is parallel to the x-axis and B is the stringer length 
+stringer_thicknesses=[0.003,0.003,0.003,0.003,0.003,0.003,0.003,0.003] #m 
+
+#if there is no stringer from a certain point, watch out, don't input zeros, just remove the last value from x_data and output
 
 def column_buckling_along(ribs,normal_stresses,stringer_area,stringer_ratios,stringer_thicknesses):
     output=[]
     x_data=[0]
     for i in range(len(ribs)):
         if i == len(ribs) - 1:
-            boundary_condition = 0.25
             length=b-ribs[-1]
         else:
-            boundary_condition = 4
             length=ribs[i+1]-ribs[i]
+        boundary_condition = 4
         x_data.append(length+x_data[-1])
         I_xx=stringer_Ixx(stringer_area[i], stringer_ratios[i], stringer_thicknesses[i])
-        value=column_buckling(length, boundary_condition, I_xx, stringer_area[i], E=72.4*10**9)
-        output.append(value/normal_stresses[i]) #returns buckling safety factor
-        print(length)
+        value=column_buckling(length, boundary_condition, I_xx, stringer_area[i], E=72.4*10**9)*10**(-6)
+        print(value)
+        safety=value/np.abs(normal_stresses[i])
+        if safety>20:safety=20
+        output.append(safety) #returns buckling safety factor
     x_data.pop(-1)
     graph = sp.interpolate.interp1d(x_data,output,kind="previous",bounds_error=False,fill_value=(output[0], output[-1]))
     x_plot = np.linspace(0, b, 300)
@@ -53,8 +56,9 @@ def column_buckling_along(ribs,normal_stresses,stringer_area,stringer_ratios,str
     plt.ylabel("Column Buckling Margin Of Safety[-]")
     plt.grid(True)
     plt.show()
+    return output
 
 
-column_buckling_along(ribs,normal_stresses,stringer_area,stringer_ratios,stringer_thicknesses) #
-
+letter= column_buckling_along(ribs,normal_stresses,stringer_area,stringer_ratios,stringer_thicknesses) #
+print((letter))
 
