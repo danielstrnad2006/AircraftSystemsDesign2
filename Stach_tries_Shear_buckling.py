069092507bb07ef2dbfb1e514d5ff07eac9909fe
danielstrnad2006 <Daniel.Strnad2006@gmail.com>
2026-01-05 14:43:48 +0100

    integration of Shear buckling started

--- Stach_tries_Shear_buckling.py
+++ Stach_tries_Shear_buckling.py
@@ -2,95 +2,90 @@ import math
 from planform import * 
 import scipy as sp
 
-wing = WingSizing(S_w=149.9, b=32.1632, c_root=7.2855, c_tip=2.0372, taper_ratio=0.2796,
-                 leading_sweep=37.537, quart_sweep=34.4871, dihedral=5)
+def Shear_buckling(ribs_input_lst, shear_stresses_input_lst, thicknesses_input):
 
-k_s_values = [
-        (1, 1.05, 15),
-        (1.05, 1.1, 13.8),
-        (1.1, 1.2, 13.2),
-        (1.2, 1.3, 12.6),
-        (1.3, 1.4, 12),
-        (1.4, 1.5, 11.8),
-        (1.5, 1.6, 11.5),
-        (1.6, 1.7, 11.2),
-        (1.7, 1.8, 10.9),
-        (1.8, 1.9, 10.7),
-        (1.9, 2.0, 10.5),
-        (2.0, 2.1, 10.4),
-        (2.1, 2.2, 10.2),
-        (2.2, 2.3, 10.0),
-        (2.3, 2.4, 9.8),
-        (2.4, 2.5, 9.75),
-        (2.5, 2.6, 9.7),
-        (2.6, 3.1, 9.6),
-        (3.1, 3.3, 9.55),
-        (3.3, 3.6, 9.5),
-        (3.6, 5.0, 9.4) 
-    ]
+    ribs= ribs_input_lst  #rib locations along span of beam
+    shear_stresses= shear_stresses_input_lst  #maximum shear stress along that section between the ribs MPA
+    thicknesses= [thicknesses_input*1e-3] * (len(ribs)+1) #metres
 
-def aspect_to_K(aspect_ratio):
-    for i in k_s_values:
-        if i[0] <= aspect_ratio < i[1]:
-            return i[2]
-        elif aspect_ratio>=k_s_values[-1][1]:
-            return 9.4
-        elif aspect_ratio<k_s_values[0][0]:
-            return 15
+    wing = WingSizing(S_w=149.9, b=32.1632, c_root=7.2855, c_tip=2.0372, taper_ratio=0.2796,
+                    leading_sweep=37.537, quart_sweep=34.4871, dihedral=5)
+
+    k_s_values = [
+            (1, 1.05, 15),
+            (1.05, 1.1, 13.8),
+            (1.1, 1.2, 13.2),
+            (1.2, 1.3, 12.6),
+            (1.3, 1.4, 12),
+            (1.4, 1.5, 11.8),
+            (1.5, 1.6, 11.5),
+            (1.6, 1.7, 11.2),
+            (1.7, 1.8, 10.9),
+            (1.8, 1.9, 10.7),
+            (1.9, 2.0, 10.5),
+            (2.0, 2.1, 10.4),
+            (2.1, 2.2, 10.2),
+            (2.2, 2.3, 10.0),
+            (2.3, 2.4, 9.8),
+            (2.4, 2.5, 9.75),
+            (2.5, 2.6, 9.7),
+            (2.6, 3.1, 9.6),
+            (3.1, 3.3, 9.55),
+            (3.3, 3.6, 9.5),
+            (3.6, 5.0, 9.4) 
+        ]
+
+    def aspect_to_K(aspect_ratio):
+        for i in k_s_values:
+            if i[0] <= aspect_ratio < i[1]:
+                return i[2]
+            elif aspect_ratio>=k_s_values[-1][1]:
+                return 9.4
+            elif aspect_ratio<k_s_values[0][0]:
+                return 15
+            
+
+    def shear_buckling_along(ribs, shear_stresses, thicknesses):
+        output=[]
+        x_data=[0]
+        poisson = 0.33
+        E_mod = 72.4*10**9
+        for i in range(len(ribs)):
+            if i == len(ribs) - 1:
+                length=wing.b/2-ribs[-1]
+            else:
+                length=ribs[i+1]-ribs[i]
+            x_data.append(length+x_data[-1])
+            if i!=0:
+                b_spar =  0.14 * wing.chord(ribs[i-1])  #conservative assumption, takes the chord length at that location
+            else: b_spar =0.14*wing.chord(0)
+            aspect_ratio = (length/b_spar)
+            K_value=aspect_to_K(aspect_ratio)
+            tau_crit = (math.pi**2*K_value*E_mod/(12*(1-poisson**2))*(thicknesses[i]/b_spar)**2)*10**(-6)
+            safety=tau_crit/shear_stresses[i]
+            if safety>20:safety=20
+            output.append(safety)
+        x_data.pop(-1)
         
+        graph = sp.interpolate.interp1d(x_data,output,kind="previous",bounds_error=False,fill_value=(output[0], output[-1]))
+        return output, graph
 
-def shear_buckling_along(ribs, shear_stresses, thicknesses):
-    output=[]
-    x_data=[0]
-    poisson = 0.33
-    E_mod = 72.4*10**9
-    for i in range(len(ribs)):
-        if i == len(ribs) - 1:
-            length=wing.b/2-ribs[-1]
-        else:
-            length=ribs[i+1]-ribs[i]
-        x_data.append(length+x_data[-1])
-        if i!=0:
-            b_spar =  0.14 * wing.chord(ribs[i-1])  #conservative assumption, takes the chord length at that location
-        else: b_spar =0.14*wing.chord(0)
-        aspect_ratio = (length/b_spar)
-        K_value=aspect_to_K(aspect_ratio)
-        tau_crit = (math.pi**2*K_value*E_mod/(12*(1-poisson**2))*(thicknesses[i]/b_spar)**2)*10**(-6)
-        safety=tau_crit/shear_stresses[i]
-        if safety>20:safety=20
-        output.append(safety)
-    x_data.pop(-1)
+    ##input all of this to find the graphs for safety buckling 
+
+
+    safety_Factor=1.2
+    count=50
+    tolerance=0.05
+    for _ in range(count):
+        values, useless = shear_buckling_along(ribs, shear_stresses, thicknesses)
+
+        for k, MS in enumerate(values):
+
+            if MS > safety_Factor + tolerance:
+                thicknesses[k] -= 0.001
+            elif MS < safety_Factor:
+                thicknesses[k] += 0.001
+
     
-    graph = sp.interpolate.interp1d(x_data,output,kind="previous",bounds_error=False,fill_value=(output[0], output[-1]))
-    return output, graph
-
-##input all of this to find the graphs for safety buckling 
-ribs= [0, 2, 4, 6, 7.03893, 9, 12]  #rib locations along span of beam
-shear_stresses= [np.float64(1334.2858661244402), np.float64(1142.9765962870895), np.float64(949.8923116195851), np.float64(765.7974376040061), np.float64(675.4898938320113), np.float64(457.8899650787215), np.float64(177.3924126996869)]  #maximum shear stress along that section between the ribs MPA
-thicknesses=[0.05,0.05,0.05,0.03,0.03,0.02,0.02] #metres
-
-
-safety_Factor=1.2
-count=50
-tolerance=0.05
-for _ in range(count):
-    values, useless = shear_buckling_along(ribs, shear_stresses, thicknesses)
-
-    for k, MS in enumerate(values):
-
-        if MS > safety_Factor + tolerance:
-            thicknesses[k] -= 0.001
-        elif MS < safety_Factor:
-            thicknesses[k] += 0.001
-
-values,graph= shear_buckling_along(ribs, shear_stresses, thicknesses)
-x_plot = np.linspace(0, wing.b/2, 300)
-sf_plot = graph(x_plot)
-plt.plot(x_plot, sf_plot)
-plt.xlabel("Spanwise location y [m]")
-plt.ylabel("Shear Buckling Margin Of Safety[-]")
-plt.grid(True)
-plt.show()
-
-print(thicknesses)
+    return shear_buckling_along(ribs, shear_stresses, thicknesses)
 
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from cross_section import *
 from planform import *
 import json
 
-from Stach_tries_Shear_buckling import shear_buckling_along as StachShearBuckling
+from Stach_tries_Shear_buckling import Shear_buckling
 
 db = 0.5
 b = 32.1632
@@ -84,88 +84,97 @@ if input("Start with detailed analysis of critical conditions? (y)")=="y":
 
 
         max_v, max_y = beam.max_deflection()
-        print(f"Maximum deflection: {max_v:.6f} at y = {max_y:.3f}")
+        print(f"Maximum deflection: {max_v:.6f} meters at y = {max_y:.3f} meters")
+
+        ### Buckling Analysis ###
+        shear_buckling_safety = Shear_buckling(ribs_locations, [tau/1e6 for tau in tau_lst], spar_thickness[0])
+        print("Shear buckling safety factors along the span:", shear_buckling_safety)
+        input("Press Enter to continue to deflection results...")
 
 
-if input("Is the final cross section chosen and do you want to proceed to verify deflection at all loading conditions? (y)")=="y":
-    crit_conds = [
-    [2.5, 103544, 138.0, 0.433, 100],   # LC-1 MTOW FL310
-    [2.5,  62767, 107.5, 0.433,   0],   # LC-2 ZFW  FL310
-    [2.5,  43807,  89.8, 0.433,   0],   # LC-3 OEW  FL310
-
-    [2.5, 103544, 138.0, 1.225, 100],   # LC-4 MTOW FL0
-    [2.5,  62767, 107.5, 1.225,   0],   # LC-5 ZFW  FL0
-    [2.5,  43807,  89.8, 1.225,   0],   # LC-6 OEW  FL0
 
 
-    # --- Negative manoeuvring @ VS1 ---
-    [-1, 103544,  87.3, 0.433, 100],    # LC-7 MTOW FL310
-    [-1,  62767,  67.7, 0.433,   0],    # LC-8 ZFW  FL310
-    [-1,  43807,  56.8, 0.433,   0],    # LC-9 OEW  FL310
 
-    [-1, 103544,  87.3, 1.225, 100],    # LC-10 MTOW FL0
-    [-1,  62767,  67.7, 1.225,   0],    # LC-11 ZFW  FL0
-    [-1,  43807,  56.8, 1.225,   0],    # LC-12 OEW  FL0
-
-
-    # --- Positive manoeuvring @ VD ---
-    [2.5, 103544, 163.0, 0.433, 100],   # LC-13 MTOW FL310
-    [2.5,  62767, 163.0, 0.433,   0],   # LC-14 ZFW  FL310
-    [2.5,  43807, 163.0, 0.433,   0],   # LC-15 OEW  FL310
-
-    [2.5, 103544, 308.7, 1.225, 100],   # LC-16 MTOW FL0
-    [2.5,  62767, 308.7, 1.225,   0],   # LC-17 ZFW  FL0
-    [2.5,  43807, 308.7, 1.225,   0],   # LC-18 OEW  FL0
-
-
-    # --- Positive manoeuvring @ VC ---
-    [2.5, 103544, 154.0, 0.433, 100],   # LC-19 MTOW FL310
-    [2.5,  62767, 154.0, 0.433,   0],   # LC-20 ZFW  FL310
-    [2.5,  43807, 154.0, 0.433,   0],   # LC-21 OEW  FL310
-
-    [2.5, 103544, 154.0, 1.225, 100],   # LC-22 MTOW FL0
-    [2.5,  62767, 154.0, 1.225,   0],   # LC-23 ZFW  FL0
-    [2.5,  43807, 154.0, 1.225,   0]    # LC-24 OEW  FL0
-    ]
-    twist_tip_lst = []
-    deflection_tip_lst = []
-
-    for cond in crit_conds:
-        print("going through load case with load factor: ", cond[0], ", mass", cond [1], "kg, Equivalent Air Speed: ", cond[2], "m/s, and density", cond[3], "kg/m^3")
-        internal_properties.set_conditions(load_factor=cond[0], weight=cond[1], v_EAS=cond[2], rho=cond[3], fuel_percentage=cond[4])
-        #internal_properties.get_internal_plot()
-        #internal_properties.get_internal_torsion_plot()
-
-        #twist_plot(internal_properties, CENTROID_X, J_P, b, db)
-        moment_distribution = internal_properties.internal_bending
-        torsion_noT_distribution = internal_properties.internal_torsion_noT
-        torsion_fullT_distribution = internal_properties.internal_torsion_fullT
-
-        #PLOT BEAM DEFLECTION
-        beam = BeamDeflection(b, db, moment_distrib=moment_distribution)
-        beam.assignI_XX(I_XX)
-        #beam.plot()
-
-        #PLOT BEAM TWIST
-        beam_twist_noT = TorsionDeflection(b, db, torsion_distrib=torsion_noT_distribution, J_distrib=internal_properties.J)
-        beam_twist_fullT = TorsionDeflection(b, db, torsion_distrib=torsion_fullT_distribution, J_distrib=internal_properties.J)
-        #plotTwists(theta_fullT=beam_twist_fullT.theta, theta_noT=beam_twist_noT.theta)
-        print("At load case with load factor: ", cond[0], ", mass", cond [1], "kg, Equivalent Air Speed: ", cond[2], "m/s, and density", cond[3], "kg/m^3")
-        print("The Reaction Shear Force is:", internal_properties.internal_shear(0))
-        print("The Reaction Bending Moment is:", internal_properties.internal_bending(0))
-        print("The Reaction Torsion Moment at full throttle is:", internal_properties.internal_torsion_fullT(0))
-        print("The Reaction Torsion Moment at zero throttle is:", internal_properties.internal_torsion_noT(0))
-        print()
-        print("The tip deflection is:", beam.v(16.0816)* 1e15, "mm")
-        deflection_tip_lst.append(beam.v(16.0816)* 1e12)
-        print("The tip twist (zero T) is:", beam_twist_noT.theta(16.0816)*180/np.pi, "deg")
-        print("The tip twist (full T) is:", beam_twist_fullT.theta(16.0816)*180/np.pi, "deg")
-        if beam_twist_fullT.theta(16.0816)>0:
-            twist_tip_lst.append(max(beam_twist_fullT.theta(16.0816)*180/np.pi, beam_twist_noT.theta(16.0816)*180/np.pi))
-        else: 
-            twist_tip_lst.append(min(beam_twist_noT.theta(16.0816)*180/np.pi, beam_twist_fullT.theta(16.0816)*180/np.pi))
-        print("-----------------------------------------------------")
-        print()
-        print()
-    print("The tip deflections for all loading conditions in m are:", deflection_tip_lst)
-    print("The tip twists for all loading conditions in deg are:", twist_tip_lst)
+#if input("Is the final cross section chosen and do you want to proceed to verify deflection at all loading conditions? (y)")=="y":
+#    crit_conds = [
+#    [2.5, 103544, 138.0, 0.433, 100],   # LC-1 MTOW FL310
+#    [2.5,  62767, 107.5, 0.433,   0],   # LC-2 ZFW  FL310
+#    [2.5,  43807,  89.8, 0.433,   0],   # LC-3 OEW  FL310
+#
+#    [2.5, 103544, 138.0, 1.225, 100],   # LC-4 MTOW FL0
+#    [2.5,  62767, 107.5, 1.225,   0],   # LC-5 ZFW  FL0
+#    [2.5,  43807,  89.8, 1.225,   0],   # LC-6 OEW  FL0
+#
+#
+#    # --- Negative manoeuvring @ VS1 ---
+#    [-1, 103544,  87.3, 0.433, 100],    # LC-7 MTOW FL310
+#    [-1,  62767,  67.7, 0.433,   0],    # LC-8 ZFW  FL310
+#    [-1,  43807,  56.8, 0.433,   0],    # LC-9 OEW  FL310
+#
+#    [-1, 103544,  87.3, 1.225, 100],    # LC-10 MTOW FL0
+#    [-1,  62767,  67.7, 1.225,   0],    # LC-11 ZFW  FL0
+#    [-1,  43807,  56.8, 1.225,   0],    # LC-12 OEW  FL0
+#
+#
+#    # --- Positive manoeuvring @ VD ---
+#    [2.5, 103544, 163.0, 0.433, 100],   # LC-13 MTOW FL310
+#    [2.5,  62767, 163.0, 0.433,   0],   # LC-14 ZFW  FL310
+#    [2.5,  43807, 163.0, 0.433,   0],   # LC-15 OEW  FL310
+#
+#    [2.5, 103544, 308.7, 1.225, 100],   # LC-16 MTOW FL0
+#    [2.5,  62767, 308.7, 1.225,   0],   # LC-17 ZFW  FL0
+#    [2.5,  43807, 308.7, 1.225,   0],   # LC-18 OEW  FL0
+#
+#
+#    # --- Positive manoeuvring @ VC ---
+#    [2.5, 103544, 154.0, 0.433, 100],   # LC-19 MTOW FL310
+#    [2.5,  62767, 154.0, 0.433,   0],   # LC-20 ZFW  FL310
+#    [2.5,  43807, 154.0, 0.433,   0],   # LC-21 OEW  FL310
+#
+#    [2.5, 103544, 154.0, 1.225, 100],   # LC-22 MTOW FL0
+#    [2.5,  62767, 154.0, 1.225,   0],   # LC-23 ZFW  FL0
+#    [2.5,  43807, 154.0, 1.225,   0]    # LC-24 OEW  FL0
+#    ]
+#    twist_tip_lst = []
+#    deflection_tip_lst = []
+#
+#    for cond in crit_conds:
+#        print("going through load case with load factor: ", cond[0], ", mass", cond [1], "kg, Equivalent Air Speed: ", cond[2], "m/s, and density", cond[3], "kg/m^3")
+#        internal_properties.set_conditions(load_factor=cond[0], weight=cond[1], v_EAS=cond[2], rho=cond[3], fuel_percentage=cond[4])
+#        #internal_properties.get_internal_plot()
+#        #internal_properties.get_internal_torsion_plot()
+#
+#        #twist_plot(internal_properties, CENTROID_X, J_P, b, db)
+#        moment_distribution = internal_properties.internal_bending
+#        torsion_noT_distribution = internal_properties.internal_torsion_noT
+#        torsion_fullT_distribution = internal_properties.internal_torsion_fullT
+#
+#        #PLOT BEAM DEFLECTION
+#        beam = BeamDeflection(b, db, moment_distrib=moment_distribution)
+#        beam.assignI_XX(I_XX)
+#        #beam.plot()
+#
+#        #PLOT BEAM TWIST
+#        beam_twist_noT = TorsionDeflection(b, db, torsion_distrib=torsion_noT_distribution, J_distrib=internal_properties.J)
+#        beam_twist_fullT = TorsionDeflection(b, db, torsion_distrib=torsion_fullT_distribution, J_distrib=internal_properties.J)
+#        #plotTwists(theta_fullT=beam_twist_fullT.theta, theta_noT=beam_twist_noT.theta)
+#        print("At load case with load factor: ", cond[0], ", mass", cond [1], "kg, Equivalent Air Speed: ", cond[2], "m/s, and density", cond[3], "kg/m^3")
+#        print("The Reaction Shear Force is:", internal_properties.internal_shear(0))
+#        print("The Reaction Bending Moment is:", internal_properties.internal_bending(0))
+#        print("The Reaction Torsion Moment at full throttle is:", internal_properties.internal_torsion_fullT(0))
+#        print("The Reaction Torsion Moment at zero throttle is:", internal_properties.internal_torsion_noT(0))
+#        print()
+#        print("The tip deflection is:", beam.v(16.0816)* 1e15, "mm")
+#        deflection_tip_lst.append(beam.v(16.0816)* 1e12)
+#        print("The tip twist (zero T) is:", beam_twist_noT.theta(16.0816)*180/np.pi, "deg")
+#        print("The tip twist (full T) is:", beam_twist_fullT.theta(16.0816)*180/np.pi, "deg")
+#        if beam_twist_fullT.theta(16.0816)>0:
+#            twist_tip_lst.append(max(beam_twist_fullT.theta(16.0816)*180/np.pi, beam_twist_noT.theta(16.0816)*180/np.pi))
+#        else: 
+#            twist_tip_lst.append(min(beam_twist_noT.theta(16.0816)*180/np.pi, beam_twist_fullT.theta(16.0816)*180/np.pi))
+#        print("-----------------------------------------------------")
+#        print()
+#        print()
+#    print("The tip deflections for all loading conditions in m are:", deflection_tip_lst)
+#    print("The tip twists for all loading conditions in deg are:", twist_tip_lst)
+#
